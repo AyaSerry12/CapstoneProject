941b790aa97a9cad318a036b91b27f96e7464371
Aya Serry <aya.serry@valeo.com>
2019-10-25 11:54:43 +0200

    Kubernets COnfig Files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,21 +1,9 @@
-#!/usr/bin/env bash
+#sudo kubectl run appx --image=ayaserry/ml_project:latest --port=80
+#sudo kubectl get pods
+#sudo kubectl expose pod appx-c8897dc55-nwngv --type=NodePort --port=8000 --target-port=80
 
-# This tags and uploads an image to Docker Hub
 
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
-dockerpath=ayaserry/ml_project
 
-# Step 2
-# Run the Docker Hub container with kubernetes
-sudo kubectl run appx --image=$dockerpath:latest --port=80
-
-# Step 3:
-# List kubernetes pods
-sudo kubectl get pods
-
-# Step 4:
-# Forward the container port to a host
-sudo kubectl expose pod appx-c8897dc55-nwngv --type=NodePort --port=8000 --target-port=80
-# sudo kubectl port-forward deployment/appx 8000:80
+kubectl create -f ./kubernetes_config/k8s_deployment_config.yml
+kubectl create -f ./kubernetes_config/k8s_service_config.yml
+kubectl get all
